d174805e3ad61fc522ff5146eefd23dd60db9c32
shrishrajGupta <shrishrajgupta54@gmail.com>
2022-11-16 21:55:46 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,27 @@
+let sr = "saurabh kumar";
+console.log(sr);
+document.getElementById("sr");
+
+function init(){
+    const currDate= new Date(2022,5,5,10);
+    if(currDate<12){
+        console.log("Good Morning");   
+    }else if(currDate>12){
+        console.log("Good After Noon");
+    }else{
+        console.log("Good Night");
+    }
+}
+
+let srs = "Shrish " 
+console.log(srs);
+function init(){
+    const currDate= new Date(2022,5,5,10);
+    if(currDate<12){
+        console.log("Good Morning");   
+    }else if(currDate>12){
+        console.log("Good After Noon");
+    }else{
+        console.log("Good Night");
+    }
+}
